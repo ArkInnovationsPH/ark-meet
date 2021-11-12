df35cd6dbebe4fff9d64295a1fedfe3dd2d42e36
Vincent-Levi <vincent@kradev.com>
2021-11-12 13:42:41 +0800

    minimize details to maximize video player

--- js/functions.js
+++ js/functions.js
@@ -21,6 +21,8 @@ $('.toggle-lesson-list').click(function() {
         $(content).removeClass('minimized')
         $(content).addClass('maximized')
 
+        $('.lessons-panel').removeClass('col-md-2').addClass('col-md-4')
+        $('.video-player').removeClass('col-md-12').removeClass('col-md-10').addClass('col-md-8')      
     } else{
         //minimizes lessons-panel
         console.log('minimiiize')
@@ -36,13 +38,14 @@ $('.toggle-lesson-list').click(function() {
 
     if(!$('.toggle-lesson-list').is('.maximized')){
         //minimized all
-          $('.lessons-panel').removeClass('col-md-4').addClass('col-md-2')
-          $('.video-player').removeClass('col-md-8').addClass('col-md-10')      
+          $('.lessons-panel').removeClass('col-md-4').addClass('col-md-2').addClass('hide')
+          $('.video-player').removeClass('col-md-8').addClass('col-md-12')      
     }else{
-        $('.lessons-panel').removeClass('col-md-2').addClass('col-md-4')
-        $('.video-player').removeClass('col-md-10').addClass('col-md-8')
+        $('.lessons-panel').removeClass('col-md-2').addClass('col-md-4').removeClass('hide')
+        $('.video-player').removeClass('col-md-12').addClass('col-md-8')
 
     }
+
 
 })
 
@@ -52,6 +55,42 @@ $('.lesson-name').click(function(){
     $('#current-lesson').text($(this).text())
 })
 
+$('.side-menu-btn').click(function() {
+    $('.lessons-panel').removeClass('hide')
+    $('.video-player').removeClass('col-md-12').addClass('col-md-10')
+})
+
+$('.toggle-subject-details').click(function() {
+    console.log('test')
+    let parent = $(this).parent().parent();
+        let content = `.${$(this).attr('content')}`;
+
+    if(parent.hasClass('minimized')){
+        //expands lessons-panel
+        $('.iframe').css({
+            height: '68vh'
+        })
+     
+        $(this).removeClass('minimized')
+        $(this).addClass('maximized')
+
+        $(this).attr('minimized', 'false')
+        $(content).removeClass('minimized')
+        $(content).addClass('maximized')
+
+    } else{
+        //minimizes lessons-panel
+        $('.iframe').css({
+            height: '84vh'
+        })
+        $(this).addClass('minimized')
+        $(this).removeClass('maximized')
+
+        $(this).attr('minimized', 'true')
+        $(content).addClass('minimized')
+        $(content).removeClass('maximized')
+    }
+})
 
 $('.show-reading-materials').click(function() {
     let target = $(this).attr('target')
